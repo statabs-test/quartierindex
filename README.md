bf7f141fdc1565aaa4e4a09662338f7d0241597a
Denis Augsburger <d.augsbi@gmail.com>
2018-01-28 15:32:15 +0100

    CSV to json with normalization of data structure

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Web-application to compare different districts in basel (Switzerland), based on 
 npm install
 npm start
 
+## Convert CSV to normalized data
+npm run setup
+
+This file uses setup/convert.js to convert the file. Currently change filename there if necessary.
+Important to note is, the expected header sorting:
+Publikationsjahr,Indikator_Nr,jahr_num,jahr_char,Wert,Indikator_Name,Wohnviertel_id,Wohnviertel,Rang
+
+The header names will be replaced and normalized, compare with output in directory data/normalized/*.json
---
+++ setup/convert.js
@@ -0,0 +1,103 @@
+const csvjson = require('csvjson');
+const fs = require('fs');
+const path = require('path');
+const jsonFile = require('jsonfile');
+const _ = require('lodash');
+
+/*
+ ###############################
+ Convert CSV to JS-Object
+ ###############################
+ */
+const data = fs.readFileSync(path.join(__dirname, '../data/spiderranks_2017.csv'), { encoding : 'utf8'});
+
+const options = {
+  delimiter : ',', // optional
+
+  // Rename column header, original was
+  // Publikationsjahr,Indikator_Nr,jahr_num,jahr_char,Wert,Indikator_Name,Wohnviertel_id,Wohnviertel,Rang
+  headers: 'publication,indicatorId,year,yearChar,value,indicatorName,districtId,district,ranking',
+};
+
+const dataObject = csvjson.toObject(data, options);
+const dataWithoutHeader =  _.tail(dataObject);
+
+/*
+ ###############################
+ Export normalized indicator data
+ ###############################
+ */
+const indicatorData = _.uniqBy(
+  _.map(dataWithoutHeader, (data) => {
+    return {
+        id: data.indicatorId,
+        name: data.indicatorName,
+        publication: data.publication,
+        year: data.year
+    }
+  }),
+  'id'
+);
+
+jsonFile.writeFile(path.join(__dirname, '../data/normalized/indicator.json'), indicatorData, function(err) {
+  err ?
+    console.error(err) :
+    console.info('Successfuly wrote indicator.json file');
+});
+
+/*
+ ###############################
+ Export normalized district data
+ ###############################
+ */
+const districtData = _.uniqBy(
+  _.map(dataWithoutHeader, (data) => {
+    return {
+      id: data.districtId,
+      name: data.district,
+    }
+  }),
+  'id'
+);
+
+jsonFile.writeFile(path.join(__dirname, '../data/normalized/district.json'), districtData, function(err) {
+  err ?
+    console.error(err) :
+    console.info('Successfuly wrote district.json file');
+});
+
+/*
+ ###############################
+ Export normalized value data
+ ###############################
+ */
+let valueId = 0;
+const valueData = _.map(dataWithoutHeader, (data) => {
+  valueId = valueId + 1;
+    return {
+      id: valueId,
+      districtId: data.districtId,
+      indicatorId: data.indicatorId,
+      value: data.value,
+      ranking: data.ranking,
+    }
+});
+
+jsonFile.writeFile(path.join(__dirname, '../data/normalized/values.json'), valueData, function(err) {
+  err ?
+    console.error(err) :
+    console.info('Successfuly wrote values.json file');
+});
+
+
+/*
+ ###############################
+ Export the whole data set in json, just for reference, debugging, checking
+ ###############################
+ */
+const outputPath = path.join(__dirname, '../data/spiderranks.json');
+jsonFile.writeFile(outputPath, dataWithoutHeader, function(err) {
+  err ?
+    console.error(err) :
+    console.info('Successfuly exported spiderranks.json file');
+});
