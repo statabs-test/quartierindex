cb6e547bcf175bb543eed651283439237fac62e2
Fabrizio <fabrizio.parrillo@fhnw.ch>
2018-04-13 18:44:54 +0200

    added color, get color from lineRanking

--- src/component/chart/Chart.tsx
+++ src/component/chart/Chart.tsx
@@ -6,20 +6,36 @@ import { District } from '../../state/district/types'
 import { Line, LineChart, XAxis } from 'recharts';
 import { getSelectedIndicators } from '../../state/indicator/selectors';
 import { Indicator } from '../../state/indicator/types';
-import { getRankingDataForChart } from '../../state/observation/selectors';
+import { getLineRanking, getRankingDataForChart } from '../../state/observation/selectors';
+import { LineRank } from '../../state/observation/types';
 
 export interface Props {
   districts: District[]
   selectedIndicators: Indicator[]
   rankingData: any
+  lineRanking: LineRank[]
 }
 
-function Chart({districts, selectedIndicators, rankingData}: Props) {
+const colorToString = (rank: LineRank): string =>
+    'hsl(' + rank.color.h.toString() + ', '
+    + rank.color.s.toString() + '%,'
+    + rank.color.v.toString() + '%)';
+
+const getColor = (lineRank: LineRank[], district: District): string => {
+  const r = lineRank.find(rank => rank.objectId === district.id);
+  if (r) {
+    return colorToString(r);
+  } else {
+    return '#A0A0A0'
+  }
+};
+
+function Chart({districts, selectedIndicators, rankingData, lineRanking}: Props) {
   return (
       <LineChart width={600} height={600} data={rankingData}>
         {districts.map(
-            distrtict => (
-                <Line key={distrtict.id} type="monotone" dataKey={distrtict.name} stroke="#8884d8" activeDot={{r: 8}}/>
+            d => (
+                <Line key={d.id} type="monotone" dataKey={d.name} stroke={getColor(lineRanking, d)} activeDot={{r: 8}}/>
             ))}
         <XAxis dataKey="name"/>
       </LineChart>
@@ -29,7 +45,8 @@ function Chart({districts, selectedIndicators, rankingData}: Props) {
 const mapStateToProps = (state: Rootstate) => ({
   districts: allDistricts(state),
   selectedIndicators: getSelectedIndicators(state),
-  rankingData: getRankingDataForChart(state)
+  rankingData: getRankingDataForChart(state),
+  lineRanking: getLineRanking(state)
 
 });
 
