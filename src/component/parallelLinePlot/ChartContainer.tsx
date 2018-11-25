ce7c01f4028f9d10668f0603fa3e920a6a79c388
Parrillo Fabrizio <fabrizio.parrillo@fhnw.ch>
2018-11-25 18:16:54 +0100

    #28 #38 calculate element size with css variable

--- src/component/parallelLinePlot/ChartContainer.tsx
+++ src/component/parallelLinePlot/ChartContainer.tsx
@@ -41,27 +41,14 @@ const getLineStroke = (district: District): number => {
   return 1
 };
 
-const getParentWidth = (): number => {
-  // const parent = document.getElementById('scroll-area');
-  // console.log(parent);
-  // If one could get the width of the parent div the plot are
-  // could be resized reponsively
-  // How do you calculate this value? Try!?
-  return 730;
-};
-
 const getWidth = (indicators: Indicator[]): number => {
-  const maxIndicators = 3;
-  const initScrollArea = getParentWidth();
-  const widthOfElement = initScrollArea / maxIndicators;
-  if (indicators.length === 1) {
-    return 10;
-  } else {
-    return widthOfElement * (indicators.length - 1);
-  }
+  const elementWidth = 190;
+  const elementMargin = 5;
+  return (elementWidth + 2 * elementMargin) * (indicators.length - 1);
 };
 
 const parallelLinePlot = (props: Props) => {
+
   const { districts, rankingData, lineRanking, selectedIndicators,
     highlightDistrict, hideDistrict, onHover, offHover } = props;
 
@@ -96,25 +83,25 @@ const parallelLinePlot = (props: Props) => {
 }
 
 const barPlot = (props: Props) => {
-  console.log(props.rankingData); 
+  console.log(props.rankingData);
   const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
+    { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+    { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+    { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+    { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+    { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+    { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+    { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+  ];
   return (
-  <div className="bar-plot-container">{props.rankingData.map(ranking =>
-    (<div className="bar-plot">
-    <BarChart layout="vertical" width={230} height={300} data={data}>
-         <Bar dataKey="uv" fill="#8884d8"/>
-       </BarChart>
-    </div>))}</div>
+    <div className="bar-plot-container">{props.rankingData.map(ranking =>
+      (<div className="bar-plot">
+        <BarChart layout="vertical" width={230} height={300} data={data}>
+          <Bar dataKey="uv" fill="#8884d8" />
+        </BarChart>
+      </div>))}</div>
   )
- 
+
 }
 
 const ChartContainer = (props: Props) => {
