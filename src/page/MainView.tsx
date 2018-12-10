eb791dcd8a6a398ffbf57bf0f850e2691e6eafba
Nicolas Mauchle <nmauchle@gmail.com>
2018-12-10 18:13:38 +0100

    Fix #57

--- src/page/MainView.tsx
+++ src/page/MainView.tsx
@@ -13,8 +13,8 @@ const MainView: React.StatelessComponent<Props> = ({}) => {
   return (
     <div className="App main-view">
       <div className="left-grid information">
-        Weitere Anpassungen können entweder direkt an den Knöpfen und Reglern oberhalb der Grafiken
-        oder über den entsprechenden Arbeitsschritten vorgenommen werden.
+      Weitere Anpassungen können entweder direkt an den Knöpfen und Reglern oberhalb der Grafiken
+      oder über den entsprechenden Arbeistsschritt vorgenommen werden.
       </div>
       <div className="left-grid title">
         <h2 className="district-ranking-title">Rangliste</h2>
