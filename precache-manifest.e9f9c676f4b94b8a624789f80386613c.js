498e0af28d091de1505fb2440725c203fd951402
Nicolas Mauchle <nicolas@nicolasmauchle.ch>
2018-12-16 16:08:21 +0100

    Updates

---
+++ precache-manifest.e9f9c676f4b94b8a624789f80386613c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "516a5680be40184e6c24b03018acc2fb",
+    "url": "/quartierindex/static/media/welcome.516a5680.jpg"
+  },
+  {
+    "revision": "fbb9092a1535159946dc",
+    "url": "/quartierindex/static/js/runtime~main.fbb9092a.js"
+  },
+  {
+    "revision": "9b3f4c69a8ac16046f9d",
+    "url": "/quartierindex/static/js/main.9b3f4c69.chunk.js"
+  },
+  {
+    "revision": "2c63ee5735d6fa59e475",
+    "url": "/quartierindex/static/js/1.2c63ee57.chunk.js"
+  },
+  {
+    "revision": "9b3f4c69a8ac16046f9d",
+    "url": "/quartierindex/static/css/main.022714e4.chunk.css"
+  },
+  {
+    "revision": "2c63ee5735d6fa59e475",
+    "url": "/quartierindex/static/css/1.50d17ec9.chunk.css"
+  },
+  {
+    "revision": "7a34d5d7c8362a2c903d54c2a94cbd3a",
+    "url": "/quartierindex/index.html"
+  }
+];
